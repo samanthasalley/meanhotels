c44fd1f286229a492a02edbf04e7361120333fb2
samantha <samanthasalley@gmail.com>
2017-03-05 06:30:54 +0000

    Update title

--- public/angular-app/hotel-list/hotel-list-controller.js
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -3,7 +3,7 @@ angular.module('meanhotel').controller('HotelsController', HotelsController);
 function HotelsController(hotelDataFactory){
     var vm = this;
     
-    vm.title = 'MEAN Hotel App';
+    vm.title = 'MEAN Hotel List';
     
     hotelDataFactory.hotelList().then(function(response){
         vm.hotels = response.data;
