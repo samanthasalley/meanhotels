808e573588085bd13e4e7948a82451827d0c57f7
samantha <samanthasalley@gmail.com>
2017-03-05 06:27:49 +0000

    Create user data factory for all user-related  requests

---
+++ public/angular-app/user-data-factory/user-data-factory.js
@@ -0,0 +1,29 @@
+angular.module('meanhotel').factory('userDataFactory', userDataFactory);
+
+function userDataFactory($http){
+    return {
+        registerUser: registerUser,
+        loginUser: loginUser,
+        updatePassword: updatePassword
+    };
+    
+    function registerUser(user){
+        return $http.post('/api/users/register', user).then(complete).catch(failed);
+    }
+    
+    function loginUser(user){
+        return $http.post('/api/users/login', user).then(complete).catch(failed);
+    }
+    
+    function updatePassword(user){
+        return $http.post('/api/users/update', user).then(complete).catch(failed);
+    }
+    
+    function complete(response){
+        return response;
+    }
+    
+    function failed(err){
+        console.log(err.statusText);
+    }
+}
