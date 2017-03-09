3ce794c4fe60786383b479137fb17f9dd6120cac
samantha <samanthasalley@gmail.com>
2017-03-09 04:03:10 +0000

    Add Google Map Embed to hotel display

--- public/angular-app/hotel-display/hotel-display-controller.js
+++ public/angular-app/hotel-display/hotel-display-controller.js
@@ -1,13 +1,24 @@
 angular.module('meanhotel').controller('HotelController', HotelController);
 
-function HotelController(hotelDataFactory, $routeParams, $route, AuthFactory, jwtHelper, $window){
+function HotelController(hotelDataFactory, $routeParams, $route, AuthFactory, jwtHelper, $window, gmkey){
     var vm = this;
     var id = $routeParams.id;
     
+    var key = gmkey;
+    var hotelAddress;
+    
     hotelDataFactory.hotelDisplay(id).then(function(response){
         vm.hotel = response.data;
+        hotelAddress = _escapeSpaces(response.data.location.address);
+        vm.mapSRC = 'https://www.google.com/maps/embed/v1/place?key=' + key + '&q=' + hotelAddress;
         vm.stars = _getStarRating(response.data.stars);
+    }).catch(function(err){
+        console.log('err', err);
     });
+    
+    function _escapeSpaces(address){
+        return encodeURI(address);
+    }
     
     function _getStarRating(stars){
         return new Array(stars);
