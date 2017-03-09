160713c4d586a88d5db177c0694f1bee881ba649
samantha <samanthasalley@gmail.com>
2017-03-09 04:00:12 +0000

    Require gulp and start gulp when app starts

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 require("./api/data/db.js");
-var express = require('express');
-var app = express();
-var path = require("path");
-var routes = require("./api/routes");
+require("./gulpfile.js");
 var bodyParser = require("body-parser");
+var express = require('express');
+var routes = require("./api/routes");
+var path = require("path");
+var gulp = require("gulp");
+var app = express();
+
+gulp.start('config'); 
+
 
 // Custom middleware to console.log data from each request
 app.use(function(req, res, next){
