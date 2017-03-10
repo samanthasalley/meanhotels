ef1f00f0531c5bc9df81c889d4034a75952d03fe
samantha <samanthasalley@gmail.com>
2017-03-10 00:00:35 +0000

    Update dburl

--- api/data/db.js
+++ api/data/db.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 var ip = process.env.IP;
-var dburl = process.env.DATABASEURL || 'mongodb://'+ip+':27017/meanhotel';
+var dburl = process.env.DATABASEURL;
 
 mongoose.connect(dburl);
 
